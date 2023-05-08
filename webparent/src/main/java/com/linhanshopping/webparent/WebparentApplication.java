768153e354efccaa8932e7376e87624f628b5076
QuanTran <trandacquan@gmail.com>
2023-05-08 22:50:07 +0700

    First Time to push to Git

---
+++ webparent/src/main/java/com/linhanshopping/webparent/WebparentApplication.java
@@ -0,0 +1,13 @@
+package com.linhanshopping.webparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebparentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebparentApplication.class, args);
+	}
+
+}
---
+++ webparent/src/test/java/com/linhanshopping/webparent/WebparentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linhanshopping.webparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebparentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

105be889213e498754be625dfabab72f0837e336
quandactran <quandactran@gmail.com>
2023-05-15 16:53:38 +0700

    Afternoon 15/05/2023

--- webparent/backend/src/main/java/com/linhanshopping/backend/user/UserRepository.java
+++ webparent/backend/src/main/java/com/linhanshopping/backend/user/UserRepository.java
@@ -1,11 +1,16 @@
 package com.linhanshopping.backend.user;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.linhanshopping.common.entity.User;
 
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends PagingAndSortingRepository<User, Integer> {
 
 	public Long countById(Integer id);
-	
+
+	@Query("SELECT u FROM User u WHERE CONCAT(u.id,' ',u.email,' ',u.firstName,' ',u.lastName) LIKE %?1%")
+	public Page<User> findAll(String keyword, Pageable pageable);
 }
